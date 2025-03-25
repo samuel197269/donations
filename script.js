7e1686b43fa0e2db66aa8384963685eaf55df7dc
samuel197269 <samune197269@gmail.com>
2025-03-25 07:47:53 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function donatePaypal() {
+    window.location.href = "https://www.paypal.com/donate?hosted_button_id=YOUR_BUTTON_ID";
+}
+
+function donateCrypto() {
+    alert("Send Bitcoin to: 3FZbgi29cpjq2GjdwV8eyHuJJnkLtktZc5");
+}
